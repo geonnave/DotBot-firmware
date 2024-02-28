bbdd38777d616a79f64a6dfb89c04c2beae94419
Geovane Fedrecheski <geonnave@gmail.com>
2024-02-28 11:44:55 +0100

    wip: add lakers with zero-touch secure join

--- projects/03app_dotbot/03app_dotbot.c
+++ projects/03app_dotbot/03app_dotbot.c
@@ -28,6 +28,11 @@
 #include "rgbled_pwm.h"
 #include "timer.h"
 #include "log_flash.h"
+#include "lakers.h"
+
+#ifdef CRYPTO_PSA
+extern void mbedtls_memory_buffer_alloc_init(uint8_t *buf, size_t len);
+#endif
 
 //=========================== defines ==========================================
 
@@ -69,6 +74,11 @@ typedef struct {
     uint8_t                  lh2_update_counter;                 ///< Counter used to track when lh2 data were received and to determine if an advertizement packet is needed
     uint64_t                 device_id;                          ///< Device ID of the DotBot
     db_log_dotbot_data_t     log_data;
+    // edhoc stuff
+    bool                     update_edhoc;                       ///< Whether EDHOC data must be processed
+    bool                     gateway_authenticated;              ///< Whether the gateway has been authenticated
+    EdhocMessageBuffer       edhoc_buffer;                       ///< Internal buffer to store received but not yet handled edhoc messages
+    uint8_t                  prk_out[SHA256_DIGEST_LEN];
 } dotbot_vars_t;
 
 //=========================== variables ========================================
@@ -85,6 +95,21 @@ static const db_rgbled_pwm_conf_t rgbled_pwm_conf = {
     }
 };
 #endif
+
+// for EDHOC
+static const uint8_t CRED_I[] = {0xA2, 0x02, 0x77, 0x34, 0x32, 0x2D, 0x35, 0x30, 0x2D, 0x33, 0x31, 0x2D, 0x46, 0x46, 0x2D, 0x45, 0x46, 0x2D, 0x33, 0x37, 0x2D, 0x33, 0x32, 0x2D, 0x33, 0x39, 0x08, 0xA1, 0x01, 0xA5, 0x01, 0x02, 0x02, 0x41, 0x2B, 0x20, 0x01, 0x21, 0x58, 0x20, 0xAC, 0x75, 0xE9, 0xEC, 0xE3, 0xE5, 0x0B, 0xFC, 0x8E, 0xD6, 0x03, 0x99, 0x88, 0x95, 0x22, 0x40, 0x5C, 0x47, 0xBF, 0x16, 0xDF, 0x96, 0x66, 0x0A, 0x41, 0x29, 0x8C, 0xB4, 0x30, 0x7F, 0x7E, 0xB6, 0x22, 0x58, 0x20, 0x6E, 0x5D, 0xE6, 0x11, 0x38, 0x8A, 0x4B, 0x8A, 0x82, 0x11, 0x33, 0x4A, 0xC7, 0xD3, 0x7E, 0xCB, 0x52, 0xA3, 0x87, 0xD2, 0x57, 0xE6, 0xDB, 0x3C, 0x2A, 0x93, 0xDF, 0x21, 0xFF, 0x3A, 0xFF, 0xC8};
+static const BytesP256ElemLen I = {0xfb, 0x13, 0xad, 0xeb, 0x65, 0x18, 0xce, 0xe5, 0xf8, 0x84, 0x17, 0x66, 0x08, 0x41, 0x14, 0x2e, 0x83, 0x0a, 0x81, 0xfe, 0x33, 0x43, 0x80, 0xa9, 0x53, 0x40, 0x6a, 0x13, 0x05, 0xe8, 0x70, 0x6b};
+// FIXME: remove this CRED_R
+static const uint8_t CRED_R[] = {0xA2, 0x02, 0x60, 0x08, 0xA1, 0x01, 0xA5, 0x01, 0x02, 0x02, 0x41, 0x0A, 0x20, 0x01, 0x21, 0x58, 0x20, 0xBB, 0xC3, 0x49, 0x60, 0x52, 0x6E, 0xA4, 0xD3, 0x2E, 0x94, 0x0C, 0xAD, 0x2A, 0x23, 0x41, 0x48, 0xDD, 0xC2, 0x17, 0x91, 0xA1, 0x2A, 0xFB, 0xCB, 0xAC, 0x93, 0x62, 0x20, 0x46, 0xDD, 0x44, 0xF0, 0x22, 0x58, 0x20, 0x45, 0x19, 0xE2, 0x57, 0x23, 0x6B, 0x2A, 0x0C, 0xE2, 0x02, 0x3F, 0x09, 0x31, 0xF1, 0xF3, 0x86, 0xCA, 0x7A, 0xFD, 0xA6, 0x4F, 0xCD, 0xE0, 0x10, 0x8C, 0x22, 0x4C, 0x51, 0xEA, 0xBF, 0x60, 0x72};
+
+// for EAD authz
+static const uint8_t ID_U[] = {0xa1, 0x04, 0x41, 0x2b};
+static const uint8_t ID_U_LEN = sizeof(ID_U) / sizeof(ID_U[0]);
+static const BytesP256ElemLen G_W = {0xFF, 0xA4, 0xF1, 0x02, 0x13, 0x40, 0x29, 0xB3, 0xB1, 0x56, 0x89, 0x0B, 0x88, 0xC9, 0xD9, 0x61, 0x95, 0x01, 0x19, 0x65, 0x74, 0x17, 0x4D, 0xCB, 0x68, 0xA0, 0x7D, 0xB0, 0x58, 0x8E, 0x4D, 0x41};
+//static const uint8_t LOC_W[] = {0x63, 0x6F, 0x61, 0x70, 0x3A, 0x2F, 0x2F, 0x65, 0x6E, 0x72, 0x6F, 0x6C, 0x6C, 0x6D, 0x65, 0x6E, 0x74, 0x2E, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72};
+static const uint8_t LOC_W[] = "http://localhost:18000";
+static const uint8_t LOC_W_LEN = (sizeof(LOC_W) / sizeof(LOC_W[0])) - 1;
+static const uint8_t SS = 2;
 
 //=========================== prototypes =======================================
 
@@ -114,6 +139,11 @@ static void radio_callback(uint8_t *pkt, uint8_t len) {
 
     // Check application is compatible
     if (header->application != DotBot) {
+        return;
+    }
+
+    // Check gateway is authenticated (only proceed if it is an edhoc message)
+    if (!_dotbot_vars.gateway_authenticated && header->type != DB_PROTOCOL_EDHOC_MSG) {
         return;
     }
 
@@ -160,6 +190,13 @@ static void radio_callback(uint8_t *pkt, uint8_t len) {
                 _dotbot_vars.control_mode = ControlAuto;
             }
         } break;
+        case DB_PROTOCOL_EDHOC_MSG:
+        {
+            uint8_t buffer_len = len - sizeof(protocol_header_t) - 2;
+            memcpy(_dotbot_vars.edhoc_buffer.content, cmd_ptr, buffer_len);
+            _dotbot_vars.edhoc_buffer.len = buffer_len;
+            _dotbot_vars.update_edhoc = true;
+        } break;
         default:
             break;
     }
@@ -188,6 +225,7 @@ int main(void) {
     _dotbot_vars.advertize           = false;
     _dotbot_vars.update_lh2          = false;
     _dotbot_vars.lh2_update_counter  = 0;
+    _dotbot_vars.update_edhoc        = false; // triggered by radio
 
     // Retrieve the device id once at startup
     _dotbot_vars.device_id = db_device_id();
@@ -199,8 +237,120 @@ int main(void) {
     db_lh2_init(&_dotbot_vars.lh2, &db_lh2_d, &db_lh2_e);
     db_lh2_start();
 
+    // Memory buffer for mbedtls
+    //#ifdef RUST_PSA
+    char buffer[4096 * 2] = {0};
+    mbedtls_memory_buffer_alloc_init(buffer, 4096 * 2);
+    //#endif
+
+    puts("Initializing EDHOC and EAD authz");
+    CredentialRPK cred_i = {0}, cred_r = {0};
+    credential_rpk_new(CRED_I, 107, &cred_i);
+    credential_rpk_new(CRED_R, 84, &cred_r);
+    EdhocInitiatorC initiator = initiator_new();
+    EdhocInitiatorWaitM2C initiator_wait_m2 = {0};
+    EdhocInitiatorProcessingM2C initiator_processing_m2 = {0};
+    EdhocInitiatorProcessedM2C initiator_processed_m2 = {0};
+    EdhocInitiatorDoneC initiator_done = {0};
+    ZeroTouchDevice device = authz_device_new(ID_U, ID_U_LEN, &G_W, LOC_W, LOC_W_LEN);
+    ZeroTouchDeviceWaitEAD2 device_wait = {0};
+    _dotbot_vars.gateway_authenticated = false;
+    int edhoc_state = 0;
+    bool begin_edhoc = false;
+    bool wait_edhoc_msg2 = false;
+    printf("Dotbot initialized.\n");
+    printf("Gateway NOT authenticated.\n");
+
     while (1) {
         __WFE();
+
+        uint8_t c_r_out;
+        if (edhoc_state == 0) {
+            edhoc_state = 1;
+            printf("Beginning handhsake...");
+            puts("computing authz_secret.");
+            BytesP256ElemLen authz_secret;
+            initiator_compute_ephemeral_secret(&initiator, &G_W, &authz_secret);
+            EADItemC ead_1;
+            authz_device_prepare_ead_1(&device, &authz_secret, SS, &device_wait, &ead_1);
+
+            EdhocMessageBuffer message_1;
+            initiator_prepare_message_1(&initiator, NULL, &ead_1, &initiator_wait_m2, &message_1);
+
+            db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_EDHOC_MSG);
+            memcpy(_dotbot_vars.radio_buffer + sizeof(protocol_header_t), message_1.content, message_1.len);
+            size_t length = sizeof(protocol_header_t) + message_1.len;
+            db_radio_disable();
+            db_radio_tx(_dotbot_vars.radio_buffer, length);
+            puts("sent msg1.");
+        } else if (_dotbot_vars.update_edhoc && edhoc_state == 1) {
+            _dotbot_vars.update_edhoc = false;
+
+            EADItemC ead_2 = {0};
+            uint8_t c_r = 0;
+            CredentialRPK fetched_cred_r = {0};
+            EdhocMessageBuffer message_2 = {0};
+            //EdhocMessageBuffer *message_2 = &(_dotbot_vars.edhoc_buffer);
+            memcpy(message_2.content, _dotbot_vars.edhoc_buffer.content, _dotbot_vars.edhoc_buffer.len);
+            message_2.len = _dotbot_vars.edhoc_buffer.len;
+            int8_t res = initiator_parse_message_2(
+                &initiator_wait_m2,
+                &message_2,
+                cred_r,
+                &initiator_processing_m2,
+                &c_r,
+                &fetched_cred_r,
+                &ead_2
+            );
+            if (res != 0) {
+                printf("Error parse msg2: %d\n", res);
+                edhoc_state = -1;
+                continue;
+            }
+
+            puts("processing ead2");
+            ZeroTouchDeviceDone device_done;
+            res = authz_device_process_ead_2(&device_wait, &ead_2, cred_r, &device_done);
+            if (res != 0) {
+                printf("Error process ead2 (authz): %d\n", res);
+                edhoc_state = -1;
+                continue;
+            }
+
+            res = initiator_verify_message_2(&initiator_processing_m2, &I, cred_i, fetched_cred_r, &initiator_processed_m2);
+            if (res != 0) {
+                printf("Error verify msg2: %d\n", res);
+                continue;
+            }
+
+            puts("preparing msg3");
+            EdhocInitiatorDoneC initiator_done;
+            EdhocMessageBuffer message_3;
+            uint8_t prk_out[SHA256_DIGEST_LEN];
+            res = initiator_prepare_message_3(&initiator_processed_m2, ByReference, NULL, &initiator_done, &message_3, prk_out);
+            if (res != 0) {
+                printf("Error prep msg3: %d\n", res);
+                continue;
+            }
+
+            db_protocol_header_to_buffer(_dotbot_vars.radio_buffer, DB_BROADCAST_ADDRESS, DotBot, DB_PROTOCOL_EDHOC_MSG);
+            memcpy(_dotbot_vars.radio_buffer + sizeof(protocol_header_t), message_3.content, message_3.len);
+            size_t length = sizeof(protocol_header_t) + message_3.len;
+            db_radio_disable();
+            db_radio_tx(_dotbot_vars.radio_buffer, length);
+            _dotbot_vars.gateway_authenticated = true;
+
+            printf("\nDotBot <-> Gateway authenticated.\n");
+            printf("Derived key:   ");
+            for (size_t i = 0; i < SHA256_DIGEST_LEN; i++) {
+                printf("%X ", _dotbot_vars.prk_out[i]);
+            }
+            printf("\n");
+        }
+
+        if (!_dotbot_vars.gateway_authenticated) {
+          continue;
+        }
 
         bool need_advertize = false;
         // Process available lighthouse data
